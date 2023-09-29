6b571748864350c9ab53c8623a0e9590e831bd3a
rogercorrente <139076362+rogercorrente@users.noreply.github.com>
2023-09-29 22:31:43 +0000

    05 - Operadores

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 var salario_bruto = 7500;
 var ir = salario_bruto * 0.10;
 var salario_liquido = salario_bruto - ir;
-console.log("Salário líquido: R$", salario_liquido);
+console.log("Salário liquido: R$", salario_liquido);
