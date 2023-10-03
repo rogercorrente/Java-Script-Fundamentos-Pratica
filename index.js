a874a034eccef137b07b830f9bf7c31da757721f
rogercorrente <139076362+rogercorrente@users.noreply.github.com>
2023-10-03 00:03:13 +0000

    08 - Comandos de Decisão Aninhados

--- index.js
+++ index.js
@@ -2,14 +2,22 @@ const prompt = require('prompt-sync')();
 
 var nome = prompt("Digite seu nome: ");
 var salario_bruto = parseFloat(prompt("Digite seu salário bruto: "));
+var dependentes = parseInt(prompt("Digite o número de dependentes: "));
 
-if (salario_bruto > 0 && salario_bruto <= 1903.98) {
-  var ir = salario_bruto * 0.05;
-} else if (salario_bruto <= 2826.65) {
-  var ir = salario_bruto * 0.075;
-} else {
-  var ir = salario_bruto * 0.15;
+var renda_percapta = salario_bruto / (dependentes + 1);
+
+if(renda_percapta >= 500){
+  if (salario_bruto > 0 && salario_bruto <= 1903.98) {
+    var ir = salario_bruto * 0.05;
+  } else if (salario_bruto <= 2826.65) {
+    var ir = salario_bruto * 0.075;
+  } else {
+    var ir = salario_bruto * 0.15;
+  }
+}else{
+  var ir = 0;
 }
+
 
 var salario_liquido = salario_bruto - ir;
 console.log("Salário liquido: R$", salario_liquido);
